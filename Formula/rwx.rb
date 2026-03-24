78e41b9b40de0fb885008dc438c1a082a82db947
Jason Robinaugh <jason@rwx.com>
2026-03-24 18:11:06 -0400

    Release 3.10.0

--- Formula/rwx.rb
+++ Formula/rwx.rb
@@ -1,20 +1,20 @@
 class Rwx < Formula
   desc "RWX is the CI platform with the best developer experience, powering the fastest builds"
   homepage "https://www.rwx.com"
-  version "3.9.4"
+  version "3.10.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/rwx-cloud/rwx/releases/download/v#{version}/rwx-darwin-x86_64", user_agent: :fake
-      sha256 "53e9cc0ff037d37eb8afd8288593c236c237a4fb2b603dd7ef2c054f17cde19a"
+      sha256 "7b1fe87a13f5069ef3ed3254cdf969fe6b190ba6144f624aae67bcbeb9fec90a"
     elsif Hardware::CPU.arm?
       url "https://github.com/rwx-cloud/rwx/releases/download/v#{version}/rwx-darwin-aarch64", user_agent: :fake
-      sha256 "111e48692b530ad6a58b2df74239f017e9eb539a8b24337e7c6a93cc1e977f73"
+      sha256 "2d3f115545951d5c884a681e765bdc41f3366b6e8641c4e3e02bd9c2d01a2552"
     end
   else
     if Hardware::CPU.intel?
       url "https://github.com/rwx-cloud/rwx/releases/download/v#{version}/rwx-linux-x86_64", user_agent: :fake
-      sha256 "476041c16332d668ffb4a18a6b1737cdf231a62533a4fe9d74c91bd952199b07"
+      sha256 "0579d85206455351aa6f9883bd78d243b718967fee57d2f9943eb80d264df702"
     end
   end
 
